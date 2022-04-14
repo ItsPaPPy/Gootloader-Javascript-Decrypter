69f83c970dbc4f0db55d09a08473c1f1387b8725
ItsPaPPy <38514524+ItsPaPPy@users.noreply.github.com>
2022-04-14 06:00:10 -0500

    Update decrypt-gootloader-js.py
    Updated code by @SometimesSleepy, due to malware operator changing their obfuscation.

--- decrypt-gootloader-js.py
+++ decrypt-gootloader-js.py
@@ -1,46 +1,61 @@
 # %%
 import re
+import math
 import sys
-
-with open(sys.argv[1],"r", encoding='utf-16-le') as f:
-    lines = f.readlines()
- 
-obfuscated = lines[0][lines[0].index("= '")+3:lines[0].index("';")]
-stringBuilder = ""
-
-for index in range(len(obfuscated)):
-    if index & 1:
-        stringBuilder += obfuscated[index]
-    else: 
-        stringBuilder = obfuscated[index] + stringBuilder
-
-pipe_split = stringBuilder.split("|");
-pipe_split = list(map(lambda x: x[::-1],pipe_split))
-
-if len(pipe_split[0]) < 200:
-    pipe_split.reverse()
 # %%
 
 
-def find_url():
-    for mal_doc in pipe_split:
-#   if "Â" in pipe_split[index]:
-#       first_split = pipe_split[index].split("Â")
-#       second_split = first_split[1].split("©")
-#       mal_doc = first_split[index]+second_split[0][::-1]+second_split[1]
-#   else:
-#       mal_doc = pipe_split[index][::-1]
-        matches = re.search('href=\"(https?:\/\/[^\"]*)', mal_doc)
-        reverse_matches = re.search('([^\"]*\/\/:s?ptth)\"=ferh', mal_doc)
-        if matches or reverse_matches:
-            if matches:
-                url = matches.group(1)
-            else:
-                url = reverse_matches.group(1)[::-1]
-            return url
+def alternating_unzip(zipped):
+    ## Imagine a reverse card shuffle. It takes every other char
+    ## and adds it to the beginning. Then takes the others and 
+    ## adds them to the end.
+    unzipped = ""
+    for index in range(len(zipped)):
+        if index & 1:
+            unzipped += zipped[index]
+        else: 
+            unzipped = zipped[index] + unzipped
+    return unzipped
 
+def find_url(mal_doc):
+    matches = re.search('href=\"(https?:\/\/[^\"]*)', mal_doc)
+    if matches:
+        return matches.group(1)
     return "No Matches Found"
 
-print(find_url())
+# %%
+with open(sys.argv[1],"r", encoding='latin1') as f:
+    obfuscated_js = f.readlines()
 
+# %%
+def method_one(obfuscated_js):
+    internal_payload = obfuscated_js[0][obfuscated_js[0].index("= '")+3:obfuscated_js[0].index("';")]
+    unzipped_payload = alternating_unzip(internal_payload)
+    if "pots|" in unzipped_payload:
+        print("Reversed String")
+        unzipped_payload=unzipped_payload[::-1]
+    else: 
+        pass
+    html_body = unzipped_payload.split("|")[0]
+    #next they encode things using "" and +
+    html_array = re.split("''|\+",html_body)
+
+    ## next every other index is reversed
+    for x in range(len(html_array)):
+        if x % 2:
+            html_array[x]=html_array[x][::-1]
+
+
+    ## last they reverse every other index
+    html_array_length = len(html_array)
+    for x in range(math.floor(html_array_length/2)):
+        if x % 2:
+            temp_holder = html_array[x]
+            html_array[x] = html_array[html_array_length-x-1]
+            html_array[html_array_length-x-1] = temp_holder
+    return "".join(html_array)
+
+url = find_url(method_one(obfuscated_js))
+print(url)
+# %%
 # %%
